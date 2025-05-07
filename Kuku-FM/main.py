dc25dc55dbc3d30d506f686f60d52aa84d910a23
Mudit Sharma <mudit1404@gmail.com>
2025-05-08 01:55:39 +0530

    Update main.py

--- Kuku-FM/main.py
+++ Kuku-FM/main.py
@@ -33,7 +33,7 @@ def clean_script_for_tts(raw_script: str) -> str:
 
 
 # Gemini setup
-genai.configure(api_key="AIzaSyBqSSu4DlfaSD4Kcpe-hQR5Px8o7e_blKA")
+genai.configure(api_key="AIzaSyAcba9ishOsQbrkNHA6Mv-DnhoPTreZPuU")
 
 st.title("🎧 KUKU Companion – Personalized Audio Stories")
 
